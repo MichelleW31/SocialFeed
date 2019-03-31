fc38c2322e1f62ca4ace1c2df7a3950020e219e5
Michelle Williams <mmwilliams@c02xw60fjhd2.eprize.net>
2019-03-30 20:57:14 -0400

    nav component build

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MyStyles from './Nav.css';
+
+const nav =  ( ) => (
+    <div className={MyStyles.Nav}>
+        <i className={MyStyles.fa_heart}></i>
+        <h2 className={MyStyles.header}>Traveler</h2>
+        <i className={MyStyles.fa_paper_plane}></i>
+    </div>
+);
+
+
+
+export default nav;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+import Nav from '../components/Nav/Nav'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <>
+        <Nav />
+        TopicFeed
+        PostFeed
+      </>
     );
   }
 }
