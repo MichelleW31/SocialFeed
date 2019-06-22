d028ed760d3f107ccfeae603f93a634e0c78fb05
Michelle Williams <mmwilliams@C02XW60FJHD2.attlocal.net>
2019-06-22 10:40:32 -0400

    start nav functionality

--- src/components/PostFeed/Post/Post.js
+++ src/components/PostFeed/Post/Post.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import MyStyles from './Post.css';
 
-const post = (props) => {
-    let imgUrl = require(`../../../../src/images/${props.recipe.imgPath}`);
+class Post extends React.Component {
+    imgUrl = require(`../../../../src/images/${this.props.recipe.imgPath}`);
 
-    return (
-        <div className={MyStyles.Post}>
-            <div className={MyStyles.Copy}>
-                <h3 className={MyStyles.Title}>{props.recipe.title}</h3>
-                <h4 className={MyStyles.Level}>Level: {props.recipe.level}</h4>
-                <h5 className={MyStyles.Time}>Cook time: {props.recipe.cooktime}</h5>
+    selectPost = () => {
+        console.log(this);
+    };
+
+    render() {
+        return(
+            <div className={MyStyles.Post} onClick={this.selectPost }>
+                <div className={MyStyles.Copy}>
+                    <h3 className={MyStyles.Title}>{this.props.recipe.title}</h3>
+                    <h4 className={MyStyles.Level}>Level: {this.props.recipe.level}</h4>
+                    <h5 className={MyStyles.Time}>Cook time: {this.props.recipe.cooktime}</h5>
+                </div>
+
+                <div className={MyStyles.ImageDiv}>
+                    <img className={MyStyles.Image} src={this.imgUrl} alt=""/>
+                </div>
+
+                <div className={MyStyles.Sendoff}>
+                    <i className={MyStyles.fa_heart}></i>
+                    <i className={MyStyles.fa_paper_plane}></i>
+                </div>
+
+                <hr/>
             </div>
-
-            <div className={MyStyles.ImageDiv}>
-                <img className={MyStyles.Image} src={imgUrl} alt=""/>
-            </div>
-
-            <div className={MyStyles.Sendoff}>
-                <i className={MyStyles.fa_heart}></i>
-                <i className={MyStyles.fa_paper_plane}></i>
-            </div>
-
-            <hr/>
-        </div>
-    )
+        )
+}
 
 };
 
-export default post;
+export default Post;
---
+++ src/components/PostView/PostView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyStyles from './PostView.css';
+
+const postview = () => {
+
+    return (
+        <div className={MyStyles.Postview}>
+            This is the postview
+        </div>
+    )
+};
+
+export default postview;
