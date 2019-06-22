1754e948171179d41ec21152e66e5c89634fc150
Michelle Williams <mmwilliams@C02XW60FJHD2.attlocal.net>
2019-06-22 10:58:51 -0400

    Updates

---
+++ precache-manifest.111c68287467328e1e7d8d92ec78cbb9.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "385734c58c0a75c23ccb5bfcc89e5e90",
+    "url": "/static/media/quesadilla.385734c5.jpg"
+  },
+  {
+    "revision": "1fbabb0ba9c83119e07c",
+    "url": "/static/css/main.4805c99d.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "207c757c8eaef969d86c",
+    "url": "/static/js/2.d83ae347.chunk.js"
+  },
+  {
+    "revision": "7dd9c7eae0813b5e74bcace0066c9b13",
+    "url": "/static/media/chicken-parmesean.7dd9c7ea.jpg"
+  },
+  {
+    "revision": "e1877ff78e382c1b24473eb10e3ddb95",
+    "url": "/static/media/deep-dish-pizza.e1877ff7.jpg"
+  },
+  {
+    "revision": "a55826c7712d1bd8ce43a71af53c0564",
+    "url": "/static/media/lamb-chops.a55826c7.jpg"
+  },
+  {
+    "revision": "fb49c4b5b86cf7d7ec9b7458ff9ea318",
+    "url": "/static/media/lentil-soup.fb49c4b5.jpg"
+  },
+  {
+    "revision": "0fdde81fd7978e7431b7e91b122c78a7",
+    "url": "/static/media/lettuce-wraps.0fdde81f.jpg"
+  },
+  {
+    "revision": "0f3a5b85140c172fc44e867b5c2a7acd",
+    "url": "/static/media/macaroni.0f3a5b85.jpg"
+  },
+  {
+    "revision": "7beb4db35c9fce0e01834cc5549f7b3a",
+    "url": "/static/media/nachos.7beb4db3.jpg"
+  },
+  {
+    "revision": "a9f63c370f54ddf06617644c12a00cfb",
+    "url": "/static/media/pesto-pasta.a9f63c37.jpg"
+  },
+  {
+    "revision": "1fbabb0ba9c83119e07c",
+    "url": "/static/js/main.b8a0d2e4.chunk.js"
+  },
+  {
+    "revision": "562b9ef89abbfa1d8a10a6176c60edfa",
+    "url": "/static/media/salmon.562b9ef8.jpg"
+  },
+  {
+    "revision": "c4e047b7b769d7f4ee165b374047fbab",
+    "url": "/static/media/shrimp.c4e047b7.jpg"
+  },
+  {
+    "revision": "5c813b0cba7bf632c822070abd0e9529",
+    "url": "/static/media/sloppy-joe.5c813b0c.jpg"
+  },
+  {
+    "revision": "faee661948167c6ebd3f8a3fd9a6f5d1",
+    "url": "/static/media/spaghetti.faee6619.jpg"
+  },
+  {
+    "revision": "e1ee57f8d6052359d691e66db7bdfb60",
+    "url": "/static/media/stir-fry.e1ee57f8.jpg"
+  },
+  {
+    "revision": "71bc575b4180f6bc28b3ff9576bd8e01",
+    "url": "/static/media/stuffed-chicken.71bc575b.jpg"
+  },
+  {
+    "revision": "fadbb810cdcd1e839027257baee6f2ef",
+    "url": "/static/media/stuffed-shells.fadbb810.jpg"
+  },
+  {
+    "revision": "3901a85b5d03e600b65338a13cf27ba9",
+    "url": "/static/media/sushi.3901a85b.jpg"
+  },
+  {
+    "revision": "5bbc9a9f4af739231a0baa8d5eec7bf0",
+    "url": "/static/media/tacos.5bbc9a9f.jpg"
+  },
+  {
+    "revision": "60a8af84f35a88be094afae5b9da7e5b",
+    "url": "/static/media/tuna-croquette.60a8af84.jpg"
+  },
+  {
+    "revision": "cd02ee311ce06ba4c6527ad4319b0b26",
+    "url": "/static/media/veggie-rice.cd02ee31.jpg"
+  },
+  {
+    "revision": "6c3f46ad4c130f95ab616f38652e640f",
+    "url": "/index.html"
+  }
+];
